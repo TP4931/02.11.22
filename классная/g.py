042f91efeea9795272d1d09bf5161deb525e6d89
user <tp364932@gmail.com>
2022-11-02 14:52:47 +0300

    g

---
+++ классная/g.py
@@ -0,0 +1,5 @@
+x=int(input("введите число"))
+if x<5 or x>10:
+    print("A")
+else:
+    print("B")
---
+++ классная/j.py
@@ -0,0 +1,7 @@
+x=int(input("введите число"))
+if x<50:
+    print("A")
+elif x< 70:
+  print("B")
+elif x< 100:
+    print("C")
